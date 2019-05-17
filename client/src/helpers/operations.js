9a67cca000616250c20f52902b2461b1acee5d7f
ktan114 <ktaneight@gmail.com>
2019-05-17 15:24:08 -0400

    Changed export default to module.exports

--- client/src/helpers/operations.js
+++ client/src/helpers/operations.js
@@ -1,15 +1,15 @@
 const arithmetic = (operation, num1, num2) => {
-    if (operation === "add") return num1 + num2;
-    else if (operation === "subtract") return num1 - num2;
-    else if (operation === "multiply") return num1 * num2;
-    else if (operation === "divide") return num1 / num2;
-}
+  if (operation === 'add') return num1 + num2;
+  else if (operation === 'subtract') return num1 - num2;
+  else if (operation === 'multiply') return num1 * num2;
+  else if (operation === 'divide') return num1 / num2;
+};
 
-const percentage = (num) => {
-    return num / 100;
-}
+const percentage = num => {
+  return num / 100;
+};
 
-export default {
-    arithmetic,
-    percentage
-}
+module.exports = {
+  arithmetic,
+  percentage,
+};
