9c8aaccb5fde98a976cad77897621e4721ff42a1
ktan114 <ktaneight@gmail.com>
2019-05-17 15:13:39 -0400

    Basic layout of calculator

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,36 @@ class App extends Component {
       total: 0,
     };
   }
-  
+
   render() {
-    return <div className="App">Calculator</div>;
+    const { total } = this.state;
+    return (
+      <div className="App">
+        Calculator
+        <div>{total}</div>
+        <div>  
+          <button>0</button>
+          <button>1</button>
+          <button>2</button>
+          <button>3</button>
+          <button>4</button>
+          <button>5</button>
+          <button>6</button>
+          <button>7</button>
+          <button>8</button>
+          <button>9</button>
+        </div>
+        <div>
+          <button>Clear</button>
+          <button>+</button>
+          <button>-</button>
+          <button>*</button>
+          <button>/</button>
+          <button>%</button>
+          <button>=</button>
+        </div>
+      </div>
+    );
   }
 }
 
